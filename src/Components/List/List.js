f2afabb70af63ac5b6483fa0f239056502b77ee5
EnWa1 <80194765+enwa1ker@users.noreply.github.com>
2023-03-22 17:32:16 +0600

    Update List.js

---
+++ src/Components/List/List.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const List = ({ tasks, searchText }) => {
+    const filteredTasks = tasks.filter(task => task.task.toLowerCase().includes(searchText.toLowerCase()));
+
+    return (
+        <ul>
+            {filteredTasks.map(task => (
+                <li key={task.id}>{task.task}</li>
+            ))}
+        </ul>
+    );
+}
+
+export default List;
