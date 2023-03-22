55b32558158aff3c860dad9b58cd4c6392ebd051
EnWa1 <80194765+enwa1ker@users.noreply.github.com>
2023-03-22 17:31:49 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,73 @@
 import { useState } from 'react';
 import './App.css';
-import Modal from './Сomponents/Modal/Modal';
-import Button from './Сomponents/Button/Button';
-import Input from "./Сomponents/Input/Input";
+import Modal from './сomponents/Modal/Modal';
+import Button from './сomponents/Button/Button';
+import List from './сomponents/List/List';
+import Input from './сomponents/Input/Input';
+
 function App() {
+    const [show, setShow] = useState(false);
+    const [searchText, setSearchText] = useState('');
+    const [addText, setAddText] = useState('');
     const [tasks, setTasks] = useState([
-        { id: 1, task: 'coding' },
-        { id: 2, task: 'eat' },
-        { id: 3, task: 'sleep' },
+        {
+            id: 1,
+            task: 'coding',
+        },
+        {
+            id: 2,
+            task: 'eat',
+        },
+        {
+            id: 3,
+            task: 'sleep',
+        },
     ]);
 
-    const [searchValue, setSearchValue] = useState('');
-    const [addValue, setAddValue] = useState('');
+    const handleShow = () => setShow(!show);
 
-    const handleSearchChange = (event) => {
-        setSearchValue(event.target.value);
+    const handleSearchTextChange = (event) => {
+        setSearchText(event.target.value);
     };
 
-    const handleAddChange = (event) => {
-        setAddValue(event.target.value);
+    const handleAddTextChange = (event) => {
+        setAddText(event.target.value);
+    };
+
+    const handleAddTask = () => {
+        const newTask = {
+            id: tasks.length + 1,
+            task: addText,
+        };
+        setTasks([...tasks, newTask]);
+        setAddText('');
+    };
+
+    const handleDelete = (taskId) => {
+        const updatedTasks = tasks.filter((task) => task.id !== taskId);
+        setTasks(updatedTasks);
     };
 
     return (
-        <div>
-            <h1>My Task List</h1>
-            <Modal>
-                <Input name="add" placeholder="Add task..." onChange={handleAddChange} value={addValue} />
-            </Modal>
-            <Input name="search" placeholder="Search tasks..." onChange={handleSearchChange} value={searchValue} />
-            <List tasks={tasks} />
+        <div className="App">
+            {show && (
+                <Modal handleShow={handleShow}>
+                    <Input
+                        name="add"
+                        placeholder="Добавьте таск"
+                        onChange={handleAddTextChange}
+                        value={addText}
+                    />
+                    <Button handleClick={handleAddTask}>Добавить таск</Button>
+                </Modal>
+            )}
+            <Button handleClick={handleShow}>Открыть модалку</Button>
+            <Input
+                name="search"
+                placeholder="Поиск"
+                onChange={handleSearchTextChange}
+            />
+            <List tasks={tasks} searchText={searchText} handleDelete={handleDelete} />
         </div>
     );
 }
