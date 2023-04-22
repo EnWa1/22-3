57fb6e374e0c20c7f6ca018c68c9f6858a46df4e
EnWa1 <80194765+enwa1ker@users.noreply.github.com>
2023-04-22 15:26:41 +0600

    Update Layout.jsx

--- DZ_ermek_22-03month5/pages/Layout/Layout.jsx
+++ DZ_ermek_22-03month5/pages/Layout/Layout.jsx
@@ -1,22 +1,39 @@
-import { Outlet } from "react-router-dom"
-import { NavLink } from "react-router-dom"
+import React from "react";
+import { Outlet, NavLink } from "react-router-dom";
 
-
-const activeLink = ({isActive}) => isActive ? 'activeLink' : ''
+const activeLink = ({ isActive }) => (isActive ? "activeLink" : "");
 
 export default function Layout() {
-    return (
-        <div className="app">
-            <nav>
-                <ul>
-                    <li><NavLink to={'/'} className={activeLink}>Main</NavLink></li>
-                    <li><NavLink to={'/about'} className={activeLink}>About</NavLink></li>
-                    <li><NavLink to={'/posts'} className={activeLink}>Posts</NavLink></li>
-                    <li><NavLink to={'/login'} className={activeLink}>Log In</NavLink></li>
-                </ul>
-            </nav>
-            <Outlet/>
-            <footer><span>EnWa1 tech</span></footer>
-        </div>
-    )
+  return (
+    <div className="app">
+      <nav className="navMenu">
+        <ul>
+          <li>
+            <NavLink to={"/"} className={activeLink}>
+              Main
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/about"} className={activeLink}>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/posts"} className={activeLink}>
+              Posts
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/login"} className={activeLink}>
+              Log In
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+      <footer className="footer">
+        <span>EnWa1 tech</span>
+      </footer>
+    </div>
+  );
 }
