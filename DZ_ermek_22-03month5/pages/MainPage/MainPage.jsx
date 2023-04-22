54801e684e29878705c2fcc0d6cecb256d914de5
EnWa1 <80194765+enwa1ker@users.noreply.github.com>
2023-04-22 15:25:09 +0600

    Update MainPage.jsx

--- DZ_ermek_22-03month5/pages/MainPage/MainPage.jsx
+++ DZ_ermek_22-03month5/pages/MainPage/MainPage.jsx
@@ -1,4 +1,23 @@
+import React from "react";
 
 export default function MainPage() {
-    return <div>Main Page</div>
+  const handleButtonClick = () => {
+    alert("Кнопка нажата!");
+  };
+
+  return (
+    <div className="main-page">
+      <h1>Добро пожаловать на главную страницу</h1>
+      <p>Здесь вы можете найти много интересного контента.</p>
+      <img src="/images/main-image.jpg" alt="Главное изображение" />
+      <ul>
+        <li>Пункт 1</li>
+        <li>Пункт 2</li>
+        <li>Пункт 3</li>
+      </ul>
+      <button className="main-page-button" onClick={handleButtonClick}>
+        Нажми на меня
+      </button>
+    </div>
+  );
 }
